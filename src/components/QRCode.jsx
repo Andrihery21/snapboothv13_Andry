9cf798d2b4afe75ff0a40f9d4dc108767f6b1674
unknown <roadsterandry@gmail.com>
2025-09-12 14:40:26 +0300

    Qr code fixed

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -4,7 +4,7 @@ import { QRCodeSVG } from 'qrcode.react';
 export const QRCode = ({ value, size = 128, imageUrl, showQROnly, qrColor = '#000000', bgColor = '#ffffff' }) => {
   const qrValue = value || imageUrl || '';
   return (
-    <div className="flex justify-center items-center">
+    <div className="w-full h-full flex justify-end items-end">
       <QRCodeSVG value={qrValue} size={size} bgColor={bgColor} fgColor={qrColor} level={"L"} />
     </div>
   );
